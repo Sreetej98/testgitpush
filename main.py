e5d57dbc91986078da2da255ac6f92de7d6587b4
Sreetej98 <vadlasreeteja98@gmail.com>
2022-07-17 19:25:52 +0530

    This is my first code push in GitHub

---
+++ main.py
@@ -0,0 +1,2 @@
+client = pymongo.MongoClient("mongodb+srv://Sreetej98:Sree9866*#@cluster0.kagw2yv.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
---
+++ monotest.py
@@ -0,0 +1,21 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Sreetej98:Sree9866*#@cluster0.kagw2yv.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+
+#import pymongo
+#client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+#db = client.test
+#print(db)
+
+d = {
+    "name" : "sreeteja",
+    "email" : "sreeteja@gmail.com",
+    "surname" : "vadla"
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
+
